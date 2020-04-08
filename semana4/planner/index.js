986013c193a998ef313ec7c7f660d3ee97175c62
Ricardo Martins de Souza Timbó <rcardms@gmail.com>
2020-04-07 21:47:05 -0300

    li com onclik

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -1,29 +1,43 @@
+function tarefaConcluida(tarefa) {
+    console.log(tarefa)
+    tarefa.style.textDecoration = "Line-through"
+}
+
+function apagarTarefas(){
+    document.getElementsByClassName("content").value= ""
+    
+}
+
 function criarTarefa() {
     const novaTarefa = document.getElementById("novaTarefa").value 
     const diaDaSemana = document.getElementById("diaDaSemana").value
- 
-        switch(diaDaSemana){
-            case "segunda":
-                document.getElementById("segunda").innerHTML += "<li>"+ novaTarefa +"</li>"
-                break;
-            case "terca":
-                document.getElementById("terca").innerHTML += "<li>"+ novaTarefa +"</li>"
-                break;
-            case "quarta":
-                document.getElementById("quarta").innerHTML += "<li>"+ novaTarefa +"</li>"
-                break;
-            case "quinta":
-                document.getElementById("quinta").innerHTML += "<li>"+ novaTarefa +"</li>"
-                break;
-            case "sexta":
-                document.getElementById("sexta").innerHTML += "<li>"+ novaTarefa +"</li>"
-                break;
-            case "sabado":
-                document.getElementById("sabado").innerHTML += "<li>"+ novaTarefa +"</li>"
-                break;
-            case "domingo":
-                document.getElementById("domingo").innerHTML += "<li>"+ novaTarefa +"</li>"
-                break;
+        if (novaTarefa.value === ""  || novaTarefa.value === " "){
+            alert("Digite uma tarefa valida!")
+        }else{
+            switch(diaDaSemana){
+                case "segunda":
+                    document.getElementById("segunda").innerHTML += "<li onclick='tarefaConcluida(this)'>"+ novaTarefa +"</li>"
+                    break;
+                case "terca":
+                    document.getElementById("terca").innerHTML += "<li onclick='tarefaConcluida(this)'>"+ novaTarefa +"</li>"
+                    break;
+                case "quarta":
+                    document.getElementById("quarta").innerHTML += "<li onclick='tarefaConcluida(this)'>"+ novaTarefa +"</li>"
+                    break;
+                case "quinta":
+                    document.getElementById("quinta").innerHTML += "<li onclick='tarefaConcluida(this)'>"+ novaTarefa +"</li>"
+                    break;
+                case "sexta":
+                    document.getElementById("sexta").innerHTML += "<li onclick='tarefaConcluida(this)'>"+ novaTarefa +"</li>"
+                    break;
+                case "sabado":
+                    document.getElementById("sabado").innerHTML += "<li onclick='tarefaConcluida(this)'>"+ novaTarefa +"</li>"
+                    break;
+                case "domingo":
+                    document.getElementById("domingo").innerHTML += "<li onclick='tarefaConcluida(this)'>"+ novaTarefa +"</li>"
+                    break;
+            }
+            document.getElementById("novaTarefa").value = ""
         }
     
     }

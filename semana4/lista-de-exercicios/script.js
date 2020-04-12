fc06ebbe588a56e88dc596a71013f659af851061
Ricardo Martins de Souza Timbó <rcardms@gmail.com>
2020-04-12 00:02:05 -0300

    concluido numero 2

--- semana4/lista-de-exercicios/script.js
+++ semana4/lista-de-exercicios/script.js
@@ -1,2 +1,6 @@
 1// temos uma função que recebe um valor numerico de dinheiro e de cotação 
 //para poder converter um valor em dolares 
+
+2// novoMontante terá um resultado de 165 (150 *1.1) e
+//o segundoMontante será default já que tesouro direto não tem no switch case
+

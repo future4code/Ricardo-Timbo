7ce019224072fc558de7d370efebfd87c11829d9
Ricardo Martins de Souza Timbó <rcardms@gmail.com>
2020-04-12 00:09:28 -0300

    concluido numero 3

--- semana4/lista-de-exercicios/script.js
+++ semana4/lista-de-exercicios/script.js
@@ -1,6 +1,14 @@
 1// temos uma função que recebe um valor numerico de dinheiro e de cotação 
 //para poder converter um valor em dolares 
 
-2// novoMontante terá um resultado de 165 (150 *1.1) e
+2// uma função que recebe 2 paramentros com uma variavel que guarda o valor final, um switch que ultiplica o valor de acordo com o investimento
+// novoMontante terá um resultado de 165 (150 *1.1) e
 //o segundoMontante será default já que tesouro direto não tem no switch case
+
+3// temos um array com 14 numeros e dois arrays vazios 
+//um for que percorre o array verificando se os numeros tem resto 0 da divisão por 2 
+// os que tem são colocados no array1, os outro são colocados no array2 
+// (Quantidade total de números 14)
+//(6)
+//(8)
 

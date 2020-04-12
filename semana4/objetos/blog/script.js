3f6aabd15e11a0f93a4c98edb50378c2a029f9b0
Ricardo Martins de Souza Timbó <rcardms@gmail.com>
2020-04-11 23:04:47 -0300

    alterações no blog

--- semana4/objetos/blog/script.js
+++ semana4/objetos/blog/script.js
@@ -1,9 +1,35 @@
-const tituloInput = document.getElementById("titulo")
-const autorInput = document.getElementById("autor")
-const conteudoInput = document.getElementById("conteudo")
+const arrayDePosts = [];
 
-const formulario =   {
-    titulo = tituloInput,
-    autor = autorInput,
-    conteudo = conteudoInput
+function criarPost(){
+    let tituloInput = document.getElementById("titulo").value
+    let autorInput = document.getElementById("autor").value
+    let conteudoInput = document.getElementById("conteudo").value
+
+
+    const objetoPost = {
+        titulo: tituloInput,
+        autor: autorInput,
+        conteudo: conteudoInput
+    }
+
+
+    arrayDePosts.push(objetoPost)
+
+    document.getElementById("titulo").value = ""
+    document.getElementById("autor").value = ""
+    document.getElementById("conteudo").value = ""
+
+    imprimePosts(arrayDePosts)
+
+}
+
+function imprimePosts(array) {
+    document.getElementById("espaco-posts").innerHTML = ""
+    for(post of array){
+        document.getElementById("espaco-posts").innerHTML += 
+        `<h3> ${post.titulo}</h3>  
+        <p> ${post.autor}</p> 
+        <p> ${post.conteudo} </p>`
+
+    }
 }

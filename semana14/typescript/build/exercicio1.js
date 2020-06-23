223a3d3bb7190655b1d1b8159c650d8dcaa9bbd7
Ricardo Martins de Souza Timbó <rcardms@gmail.com>
2020-06-22 22:50:21 -0300

    exercicio 1 e 2 completo

---
+++ semana14/typescript/build/exercicio1.js
@@ -0,0 +1,21 @@
+var ArcoIris;
+(function (ArcoIris) {
+    ArcoIris["VERMELHO"] = "Vermelho";
+    ArcoIris["LARANJA"] = "Laranja";
+    ArcoIris["AMARELO"] = "Amarelo";
+    ArcoIris["VERDE"] = "Verde";
+    ArcoIris["AZUL"] = "Azul";
+    ArcoIris["ANIL"] = "Anil";
+    ArcoIris["VIOLETA"] = "Violeta";
+})(ArcoIris || (ArcoIris = {}));
+const luana = {
+    nome: "Luana",
+    idade: 8,
+    corFavorita: ArcoIris.AZUL
+};
+const antonia = {
+    nome: "Antonia",
+    idade: 35,
+    corFavorita: ArcoIris.VERDE
+};
+//# sourceMappingURL=exercicio1.js.map

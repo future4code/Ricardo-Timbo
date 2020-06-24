4f0537d8e6ff63921c88628b2a12aabc038d76e9
Ricardo Martins de Souza Timbó <rcardms@gmail.com>
2020-06-23 23:25:21 -0300

    Exercicios da aula45

---
+++ semana14/aula45/src/index.ts
@@ -0,0 +1,52 @@
+// --------------- Exercicio 1 ----------------------------
+// function welcome(a: string, b: number): string {
+//     return `Olá ${a}! Você tem ${b} anos. Em sete anos terá ${ 7 + b } `
+// }
+
+// const nome: string = process.argv[2]
+// const idade: number = Number(process.argv[3])
+
+
+// console.log(
+//     welcome(nome, idade)
+// )
+
+//_____________________________________________________________________
+
+// --------------- Exercicio 2----------------------------
+
+// function matematica(operacao:string, b: number, c: number): string {
+//     switch (operacao) {
+//         case "add":
+//             return `Resposta; ${b + c}`;
+//         case "add":
+//             return `Resposta; ${b - c}`;
+//         case "add":
+//             return `Resposta; ${b * c}`;
+//         case "add":
+//             return `Resposta; ${b / c}`;        
+//         default:
+//             return `Resposta: Operação não encontrada`; 
+//     }
+// } 
+
+// const operacao: string = process.argv[2];
+// const primeiroNumero: number = Number(process.argv[3]);
+// const segundoNumero: number = Number(process.argv[4]);
+
+// console.log(
+//     matematica(operacao, primeiroNumero, segundoNumero)
+// )
+
+//_____________________________________________________________________
+
+// --------------- Exercicio 3 ----------------------------
+
+function listaDeTarefas (a: string, b: string): string {
+    
+}
+
+
+
+
+

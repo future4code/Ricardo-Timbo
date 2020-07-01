b76f27adf82640bbd8f3f13594dc96b33871624e
Ricardo Martins de Souza Timbó <rcardms@gmail.com>
2020-07-01 16:51:46 -0300

    exercicios aula 50

---
+++ semana15/aula50/src/index.ts
@@ -0,0 +1,11 @@
+import {Client} from "./Client"
+
+const client: Client = {
+    name: "Goli",
+    registrationNumber: 1,
+    consumedEnergy: 100,
+  
+    calculateBill: () => {
+      return 2;
+    }
+  }

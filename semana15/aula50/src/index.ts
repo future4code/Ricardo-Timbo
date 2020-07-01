c9c46d28813a8557a16f44c9e801649eb2c5b924
Ricardo Martins de Souza Timbó <rcardms@gmail.com>
2020-07-01 19:55:31 -0300

    exercicios 1 e 2

--- semana15/aula50/src/index.ts
+++ semana15/aula50/src/index.ts
@@ -1,4 +1,11 @@
+//1 
+//a) todas as propiedades, todos os atributos da interface são estão padrão 
+
+//2
+//a) não é possivel declarar CEP q ele é do tipo protected, maximo possivel é buscar uma instancia que tenha com o get
+
 import {Client} from "./Client"
+import {Place} from "./Place"
 
 const client: Client = {
     name: "Goli",
@@ -9,3 +16,12 @@ const client: Client = {
       return 2;
     }
   }
+
+console.log("Nome: ", client.name)
+console.log("Id: ", client.registrationNumber)
+console.log("Consumo: ", client.consumedEnergy)
+console.log("Calculo: ", client.calculateBill())
+
+const lugar: Place = {
+    cep: "22222222"
+}

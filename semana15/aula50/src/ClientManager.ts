7909af6bf308771ecb2782f2056521934e6c09e8
Ricardo Martins de Souza Timbó <rcardms@gmail.com>
2020-07-01 23:19:00 -0300

    exercicios 8

--- semana15/aula50/src/ClientManager.ts
+++ semana15/aula50/src/ClientManager.ts
@@ -1,7 +1,7 @@
 import { Client } from "./Client";
 
 export class ClientManager {
-    private clients : Client []
+    private clients : Client [] = []
 
     constructor() {
         this.clients = []
@@ -15,7 +15,37 @@ export class ClientManager {
         this.clients.push(client)
     } 
     calculateBillOfClient(registrationNumber: number) : number {
-        //
+        const foundClient = this.clients.find((client) =>{
+            return client.registrationNumber === registrationNumber
+        })
+        if(foundClient){
+            return foundClient.calculateBill()
+        }
+
+        return 0
+    }
+    calculeTotalIncome():  number {
+        let total: number = 0;
+        this.clients.forEach((client) =>{
+            total += client.calculateBill()
+        })
+        return total
+    }
+
+    deleteUser(registrationNumber:number): void {   
+        let registrationIndex = undefined
+
+        for (let i =0; i < this.clients.length; i++ ){
+            if(this.clients[i].registrationNumber === registrationNumber){
+                registrationIndex = i;
+            }
+        }
+
+        if (registrationIndex !== undefined) {
+            this.clients.splice(registrationIndex, 1)
+        }
+    }   
+        
     }
     
 }

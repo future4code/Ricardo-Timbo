aaa406d572e37e6fc650d37a1a9803af46ec5935
Ricardo Martins de Souza Timbó <rcardms@gmail.com>
2020-07-13 15:07:04 -0300

    erro de branch

--- semana16/projeto/src/index.ts
+++ semana16/projeto/src/index.ts
@@ -16,7 +16,7 @@ app.use(express.json());
 const server = app.listen(process.env.PORT || 3003, () => {
   if (server) {
     const address = server.address() as AddressInfo;
-    console.log(`Server is running in http://localhost:${address.port}`);
+    console.log(`Server is running on http://localhost:${address.port}`);
   } else {
     console.error(`Failure upon starting server.`);
   }
